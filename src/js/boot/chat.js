0d2869ddf1178fd97c72a449e8534a4651ed0108
Crosseye Jack <dan@crosseyejack.com>
2018-01-05 20:50:25 +0000

    Changed the behavouir of the extenstion page render. Gonna look into fixing the reported bug.

--- src/js/boot/chat.js
+++ src/js/boot/chat.js
@@ -1,16 +1,19 @@
-var match = window.location.search.match(/channel=([^&]+)(&|$)/);
+var match = window.location.search.match(/channel=([a-z0-9-_]+)(&|$)/i);
 var channel = match ? match[1].toLowerCase() : null;
-var iframe = document.querySelector('#chat');
-iframe.src = 'https://www.twitch.tv/' + channel + '/chat';
-document.querySelector('title').textContent = 'TGA: ' + channel;
+if (channel !== null)
+{
+	var iframe = document.querySelector('#chat');
+	iframe.src = 'https://go.twitch.tv/'.concat(channel).concat('/chat');
+	document.querySelector('title').textContent = 'TGA: ' + channel;
 
-// stupid chrome rendering bugs
-// If I won't do this the chat refuses to render and looks invisible, even
-// though it's loaded. Still doesn't work 100% of the time :/
-setTimeout(function () {
-	var pos = iframe.style.position;
-	iframe.style.position = 'static';
+	// stupid chrome rendering bugs
+	// If I won't do this the chat refuses to render and looks invisible, even
+	// though it's loaded. Still doesn't work 100% of the time :/
 	setTimeout(function () {
-		iframe.style.position = pos;
-	}, 100);
-}, 600);
+		var pos = iframe.style.position;
+		iframe.style.position = 'static';
+		setTimeout(function () {
+			iframe.style.position = pos;
+		}, 100);
+	}, 600);
+}
